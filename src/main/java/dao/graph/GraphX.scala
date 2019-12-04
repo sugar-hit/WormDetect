56dcf82302cdf3e9c46140ca82bd69637cb0976a
关运通 <yuntong.guan@gmail.com>
2019-12-04 15:33:50 +0800

    Function add: Graph Generator toolkit in scala.

--- src/main/java/dao/graph/GraphX.scala
+++ src/main/java/dao/graph/GraphX.scala
@@ -1,24 +1,22 @@
 package dao.graph
 
 import util.Parser
-import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 
 class GraphX {
-  def graphGenerator (javaRDD : JavaRDD[Map[String, Object]]) : Graph[String, String] = {
-    val scalaRDD: RDD[Map[String, Object]] = javaRDD.rdd
-    val vertexSrc: RDD[String] = scalaRDD.map(
+  def graphGenerator (rdd : RDD[java.util.Map[String, Object]]) : Graph[String, String] = {
+    val vertexSrc: RDD[String] = rdd.map(
       record => Parser.toStr(record.get("i_client_ip"))
     )
-    val vertexDst: RDD[String] = scalaRDD.map(
+    val vertexDst: RDD[String] = rdd.map(
       record => Parser.toStr(record.get("i_server_ip"))
     )
     val vertexRDD: RDD[String] = vertexSrc.union(vertexDst).distinct()
     val vertex: RDD[(VertexId, String)] = vertexRDD.map(
       record => new Tuple2(Parser.ip2Long(record), record)
     )
-    val edge: RDD[Edge[String]] = scalaRDD.map(
+    val edge: RDD[Edge[String]] = rdd.map(
       record =>
         new Edge[String](
           Parser.ip2Long(Parser.toStr(record.get("i_client_ip"))),
@@ -29,4 +27,15 @@ class GraphX {
     val graph: Graph[String, String] = Graph(vertex, edge)
     return graph
   }
+
+  def graphOutDegreeAVG (graph: Graph[String, String]) : Long = {
+    if (graph == null)
+      return 0
+    val nodes : Long = graph.vertices.count()
+    val outDegree : Long = graph.outDegrees.count()
+    if (nodes == 0L)
+      return outDegree
+    outDegree / nodes
+  }
+
 }
